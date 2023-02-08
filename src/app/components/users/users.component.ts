d8812138425fe6c824449ada21255ca7a3fadb4b
sara <sara.atef001@gmail.com>
2023-02-08 17:18:32 +0200

    getAllUsers

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {
-
+export class UsersComponent implements OnInit {
+  users: { id: number; email: string; password: string }[] = [];
+  constructor(public userServices: UserService) {}
+  ngOnInit(): void {
+    this.userServices.getAllUsers().subscribe((response) => {
+      console.log(typeof response);
+      this.users = response as {
+        id: number;
+        email: string;
+        password: string;
+      }[];
+    });
+  }
 }
